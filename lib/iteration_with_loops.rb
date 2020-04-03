a91086e18a8499a002f4e3dd8364ac55f627f065
Justin Langlinais <justinclanglinais@gmail.com>
2020-04-03 00:51:35 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,5 +5,6 @@ def find_even_values(src)
   row_index = 0
   while row_index < src.length do
     element_index = 0 
-    while element_index < src[row_index].count
+    while element_index < src[row_index].count do
+      
 end
